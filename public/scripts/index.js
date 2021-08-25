4fd43c430a959d6999e711c1a1d568d008ac3867
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-08-25 14:25:15 +0430

    refactor:change name app.js to index.js to work with webpack

---
+++ public/scripts/index.js
@@ -0,0 +1,31 @@
+import Hangman from "./hangman";
+import getPuzzle from "./requests";
+
+const puzzleEl = document.querySelector("#puzzle");
+const leftGuessEl = document.querySelector("#leftGuess");
+let game;
+
+window.addEventListener("keypress", (e) => {
+    const guess = String.fromCharCode(e.charCode);
+    game.makingGuess(guess);
+    render();
+});
+
+const render = () => {
+    puzzleEl.innerHTML = "";
+    leftGuessEl.textContent = game.statusMessage;
+
+    game.puzzle.forEach((letter) => {
+        puzzleEl.innerHTML += `<span>${letter}</span>`;
+    });
+};
+
+const startGame = async () => {
+    const puzzle = await getPuzzle("2");
+    game = new Hangman(puzzle, 5);
+    render();
+};
+
+document.querySelector("#reset").addEventListener("click", startGame);
+
+startGame();
